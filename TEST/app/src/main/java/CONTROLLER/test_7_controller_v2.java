b688652432687a6cee0bbaaaaff785763b5427b7
oofy-sotry <153573128+oofy-sotry@users.noreply.github.com>
2024-11-06 16:38:18 +0900

    code_change

--- TEST/app/src/main/java/CONTROLLER/test_7_controller_v2.java
+++ TEST/app/src/main/java/CONTROLLER/test_7_controller_v2.java
@@ -1,8 +1,8 @@
 package CONTROLLER;
 
-import SERVICE.test_7_service;
-import DAO.test_7_DAO;
-import DTO.test_7_DTO;
+import SERVICE.test_7_service_v2;
+import DAO.test_7_DAO_v2;
+import DTO.test_7_DTO_v2;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,25 +12,23 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet("/game")
-public class test_7_controller extends HttpServlet {
+@WebServlet("/game2")
+public class test_7_controller_v2 extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    private test_7_DAO test_7_DAO = new test_7_DAO();
+    private test_7_DAO_v2 test_7_DAO_v2 = new test_7_DAO_v2();
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
-        
-        test_7_DTO test_7_DTO = test_7_DAO.getGameModel(session);
-        
-        test_7_service gameModel = new test_7_service(test_7_DTO);
-
+        test_7_DTO_v2 test_7_DTO_v2 = test_7_DAO_v2.getGameModel(session);
+        test_7_service_v2 gameModel = new test_7_service_v2(test_7_DTO_v2);
         String playerChoice = request.getParameter("choice");
-        if (playerChoice != null && !test_7_DTO.isGameOver()) {
+
+        if (playerChoice != null && !test_7_DTO_v2.isGameOver()) {
             gameModel.playRound(playerChoice);
-            test_7_DAO.updateGameModel(session, test_7_DTO);
+            test_7_DAO_v2.updateGameModel(session, test_7_DTO_v2);
         }
 
-        request.setAttribute("test_7_DTO", test_7_DTO);
-        request.getRequestDispatcher("test_7.jsp").forward(request, response);
+        request.setAttribute("test_7_DTO_v2", test_7_DTO_v2);
+        request.getRequestDispatcher("test_7_v2.jsp").forward(request, response);
     }
 }
